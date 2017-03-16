9b570678abfb0a9e0de6d069d2f027768d2b2fc0
dcorey28 <davidmatthewcorey@gmail.com>
2017-03-16 21:32:15 +0000

    Created overall structure of project. Just need to create all movie pages.

--- routes/index.js
+++ routes/index.js
@@ -13,38 +13,33 @@ router.get('/', function(req, res, next) {
 router.get('/movie', function(req, res, next) {
 	console.log("Movie requested");
 	var title = req.query.title;
-	console.log("Title = " + title);
 	var file = "../public/movies/" + title;
-	console.log("Path = " + file);
-	console.log("Resolved Path = " + path.resolve(__dirname, file));
-	var movie_mp4, total;
+	//console.log("Range: " + req.headers.range);
+	//console.log(req);
+
 	fs.readFile(path.resolve(__dirname, file), function(err, data) {
 		if (err) {
-			//console.log(err);
 			throw err;
 		}
-		console.log("Found the movie");
-		movie_mp4 = data;
+		//console.log("Range 2: " + req.headers.range);
+		var total = data.length;
+		var range = req.headers.range;
+		var positions =  range.replace(/bytes=/, "").split("-");
+		var start = parseInt(positions[0], 10);
+		var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+		var chunksize = (end - start) + 1;
+		
+		console.log("Sending Movie Now..");
+		res.writeHead(206, {
+				"Content-Range":"bytes " + start + "-" + end + "/" + total,
+					"Accept-Ranges":"bytes",
+					"Content-Length": chunksize,
+					"Content-Type":"video/mp4"
+		});
+		res.end(data.slice(start, end + 1), "binary");
+		console.log("Movie Sent!");	
 	});
-	console.log("Break 0");
-	total = movie_mp4.length;
-	console.log("Break 1");
-	var range = req.header.range;
-	console.log("Break 2");
-	var positions =  range.replace(/bytes=/, "").split("-");
-	var start = parseInt(positions[0], 10);
-	var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-	var chunksize = (end - start) + 1;
 	
-	console.log("Sending Movie Now..");
-	res.writeHead(206, {
-			"Content-Range":"bytes " + start + "-" + end + "/" + total,
-				"Accept-Ranges":"bytes",
-				"Content-Length": chunksize,
-				"Content-Type":"video/mp4"
-	});
-	res.end(movie_mp4.slice(start, end + 1), "binary");
-	console.log("Movie Sent!");	
 });
 
 module.exports = router;
